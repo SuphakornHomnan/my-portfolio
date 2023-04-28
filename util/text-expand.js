307c6f76363ef2f5bd36652e497f87f403c7594d
nice_larngear <supakorn@larngeartech.com>
2023-04-28 19:08:37 +0700

    add truncateDescription function

--- util/text-expand.js
+++ util/text-expand.js
@@ -1,26 +1,35 @@
-import React, { useState } from "react";
+import { useState, Fragment } from "react";
+
+export const truncateDescription = ({
+  description,
+  maxLength,
+  isTruncated,
+}) => {
+  if (description === undefined || description === null) {
+    return "";
+  }
+  if (description.length <= maxLength) {
+    return description;
+  }
+
+  return isTruncated ? `${description.slice(0, maxLength)}` : description;
+};
 
 export const TextExpand = ({ text, maxLength, className }) => {
   const [isTruncated, setIsTruncated] = useState(true);
 
   const toggleTruncate = () => setIsTruncated(!isTruncated);
 
-  const getText = () => {
-    if (text.length <= maxLength) {
-      return text;
-    }
-
-    return isTruncated ? `${text.slice(0, maxLength)}` : text;
-  };
-
   const linkText = isTruncated ? "more" : "less";
   const showLink = text.length > maxLength && isTruncated;
 
   return (
     <div className={className}>
-      <p style={{ display: "inline" }}>{getText()}</p>
+      <p style={{ display: "inline" }}>
+        {truncateDescription({ description: text, maxLength, isTruncated })}
+      </p>
       {showLink && (
-        <a
+        <button
           href="#"
           onClick={toggleTruncate}
           style={{ color: "#06B2F7", marginLeft: "5px" }}
@@ -28,8 +37,39 @@ export const TextExpand = ({ text, maxLength, className }) => {
           onMouseLeave={(e) => (e.target.style.color = "#06B2F7")}
         >
           ...See {linkText}
-        </a>
+        </button>
       )}
     </div>
+  );
+};
+
+export const DescriptionExpand = ({ description }) => {
+  const [isTruncated, setIsTruncated] = useState(true);
+
+  const toggleTruncate = () => setIsTruncated(!isTruncated);
+
+  return (
+    <p className="text-xs text-zinc-50 lg:text-sm">
+      {isTruncated ? (
+        <>
+          <button
+            onClick={toggleTruncate}
+            style={{ color: "#06B2F7", marginLeft: "5px" }}
+            onMouseEnter={(e) => (e.target.style.color = "#FFFFFF")}
+            onMouseLeave={(e) => (e.target.style.color = "#06B2F7")}
+            type="button"
+          >
+            ...See More
+          </button>
+        </>
+      ) : (
+        description.split("\n").map((line, index) => (
+          <Fragment key={index}>
+            {line.trim()}
+            <br />
+          </Fragment>
+        ))
+      )}
+    </p>
   );
 };
